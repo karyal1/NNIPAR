2ddac2af47517d15f33b3fae8a77740cd74198bc
karyal1 <90630070+karyal1@users.noreply.github.com>
2022-06-13 12:54:00 -0400

    Add files via upload

--- usemodels.py
+++ usemodels.py
@@ -20,7 +20,7 @@ def nnopen(wind,chla,sza,tau,omega,alpha,ozone):
     outputscalar_open=load('soutputstdopen.pkl')
     Xopen=np.array([wind,chla,sza,tau,omega,alpha,ozone])
     Xopen=inputscalar_open.transform([Xopen])
-    a=outputscalar_open.inverse_transform(model_open.predict([Xopen])).flatten()
+    a=outputscalar_open.inverse_transform(model_open([Xopen])).flatten()
     return a
     
     
@@ -30,8 +30,7 @@ def nncoastal(wind,chla,sza,tau,omega,alpha,ozone,B,a,bb):
     outputscalar_coastal=load('s1toutputstdcoastal.pkl')
     Xcoastal=np.array([wind,chla,sza,tau,omega,alpha,ozone,B,a,bb])
     Xcoastal=inputscalar_coastal.transform([Xcoastal])
-    a=model_coastal.predict([Xcoastal])
-    a=outputscalar_coastal.inverse_transform(model_coastal.predict([Xcoastal])).flatten()
+    a=outputscalar_coastal.inverse_transform(model_coastal([Xcoastal])).flatten()
     return a
     
 def verticalprofile(x):
@@ -41,10 +40,20 @@ def verticalprofile(x):
     print(iparz)
     return iparz
     
-##Users can adjust the value of different input parameters within the given range
+# Test cases for open ocean and coastal waters to check performance of package in users' environment
+#model and enviroment are up to date when the predicted values match with reference predictions 
+testopen=[[4,0.1,74,0.4,0.9,1,400],[8,2,30,0.3,0.95,1.3,350],[2,10,55,0.1,0.97,0.5,380]]
+testcoastal=[[4,0.1,74,0.4,0.9,1,400,0.05,0.25,0.075],[8,2,30,0.3,0.95,1.3,350,0.01,1.1,0.1],[2,10,55,0.1,0.97,0.5,380,0.03,0.6,0.5]]
+
+print(nnopen(*testopen[0]),nnopen(*testopen[1]),nnopen(*testopen[2]))
+print('Reference predictions: [1.3082751e+02 4.0552534e-02 3.9834082e-01] [1.3257466e+03 2.4190046e-01 2.0054483e-01] [1.0634016e+03 4.9300832e-01 3.7214747e-01]  ')
+print(nncoastal(*testcoastal[0]),nncoastal(*testcoastal[1]),nncoastal(*testcoastal[2]))
+print('Reference predictions: [ 1.4436867e+02 -4.0723542e-03  1.7823621e+00] [1.3895797e+03 2.4155480e-01 2.3116295e+00] [8.3044104e+02 7.6482791e-01 3.2773843e+00]')
+
+##Users can adjust the value of different input parameters within the given range as needed.
 ##The parameters B, a, and bb are used only for coastal waters NN model.
 ## Function nnopen() and nncoastal() give an array of IPAR(0^-),K_1,K_2 for corresponding cases
-## function verticalprofile(x) takes output from NN model as input and can be used to get IPAR vertical profile for corresponding case
+## function verticalprofile(x) takes output from NN model's output as input and can be used to get IPAR vertical profile for corresponding case
 
 wind=4          ##wind speed (m/s), range=(0.5-10)
 chla=0.1        ##chla(mg/m3), range=(0.001,30)
@@ -57,9 +66,8 @@ B=0.05          ##Backscattering fraction at 670 nm (unitless), range=(0.002,0.0
 a=0.25          ##absorption coefficient at 490 nm (m-1), range=(0.015-1.274)
 bb=0.075        ##backscattering coefficient at 490 nm(m_1), range=(0.001-0.116)
 print(nnopen(wind,chla,sza,tau,omega,alpha,ozone)) ##prints an array of (IPAR(0^-),K_1 and K_2) for open ocean
-verticalprofile(nnopen(wind,chla,sza,tau,omega,alpha,ozone))## gives IPAR vertical profile upto from 0m to 200m with 2m resolution for open waters
+# verticalprofile(nnopen(wind,chla,sza,tau,omega,alpha,ozone))## gives IPAR vertical profile upto from 0m to 200m with 2m resolution for open waters
     
 
 print(nncoastal(wind,chla,sza,tau,omega,alpha,ozone,B,a,bb)) ####prints an array of (IPAR(0^-),K_1 and K_2) for coastal waters
-verticalprofile(nncoastal(wind,chla,sza,tau,omega,alpha,ozone,B,a,bb)) ## gives IPAR vertical profile upto from 0m to 200m with 2m resolution for coastal waters
-    
+# verticalprofile(nncoastal(wind,chla,sza,tau,omega,alpha,ozone,B,a,bb)) ## gives IPAR vertical profile upto from 0m to 200m with 2m resolution for coastal waters
